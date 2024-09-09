c30be3c99b94711a13273ca9239941a463e5f3cc
Akshay Shendre <akshay6720@gmail.com>
2024-09-09 12:29:10 +0530

    second commit

--- myapps/oauth2.py
+++ myapps/oauth2.py
@@ -1,8 +1,9 @@
-from jose import JWTError
+# from jose import JWTError
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
 from jwt.exceptions import InvalidTokenError
 from datetime import datetime, timedelta
+from . import schemas
 
 SECRET_KEY = "n3u878ecu8n9inmowjxjnnc938780238h9fy7rfny3c3bqywuqned9un97r57f76gtbyqiuhj5we46gs8hqw9yxbwdx"
 ALGORITHM = "HS256"
@@ -15,3 +16,10 @@ def create_access_token(data: dict):
   
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
+
+def verify_access_token(token: str, credential_exception):
+  payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM) 
+  id: str = payload.get('user_id') 
+  if id is None:
+    raise credential_exception
+  token_data = schemas.Token_data(id=id)
